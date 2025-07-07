2cfc7ae2821a9b2b20f92d03e190bf9fdcd1865f
pcannon09 <pcannon775@gmail.com>
2025-07-07 22:58:23 +0200

    Simple and complex debug log with even more debugging

--- inc/macros.hpp
+++ inc/macros.hpp
@@ -15,6 +15,12 @@
 #define PD_VERSION_CHECK(STT_VERSION_MAJOR, STT_VERSION_MINOR, STT_VERSION_PATCH, STT_VERSION_STATE) \
     ((PD_VERSION_MAJOR<<16)|(STT_VERSION_MINOR<<8)|(STT_VERSION_PATCH)|(STT_VERSION_STATE << 24))
 
+#define PD_DEBUGLOG(_logger, _type, _msg) \
+    _logger.log(_type, std::string(std::string(__FILE__) + ":" + std::to_string(__LINE__) + " " + _msg), 0)
+
+#define PD_SIMPLE_DEBUGLOG(_type, _msg) \
+    std::cout << _type << " [" << std::string(std::string(__FILE__) + ":" + std::to_string(__LINE__)) << "] " << _msg << "\n";
+
 // Macro utils
 #define PD_STRINGIFY(x) #x
 #define PD_TOSTRING(x) STT_STRINGIFY(x)
