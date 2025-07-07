f885ef40c993dc9da661e5b44b71a139497b802e
pcannon09 <pcannon775@gmail.com>
2025-07-07 22:58:39 +0200

    Debug level in logs

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -1,4 +1,8 @@
+#include <iostream>
+#include <string>
+
 #include "../../inc/Debug.hpp"
+#include "../../inc/macros.hpp"
 
 int main()
 {
@@ -9,6 +13,11 @@ int main()
 	debugSettings.timeZone = true;
 	debugSettings.output = true;
 	debugSettings.custom = false;
+	debugSettings.logLevelIgnoreOutput = false;
+	debugSettings.logLevelIgnoreSave = false;
+	debugSettings.logLevel = 0;
+
+	debugSettings.filePath = "./logs/log.log";
 
 	// NOTE: CAN BE USED IF WANTED, UNCOMMENT THIS CODE TO USE THE FEATURE
 	/*
@@ -21,13 +30,14 @@ int main()
 	debugSettings.totalCustom = "Custom text for debug label";
 	*/
 
-	debugSettings.filePath = "./logs/log.log";
-
 	pd::Debug debug("main-debug", debugSettings);
 
-	debug.log("information", "This is a simple test for debugging");
-	debug.log("error", "This is a simple test for debugging 2");
-	debug.log("warning", "This is a simple test for debugging 3");
+	debug.log("information", "This is a simple test for debugging (DEBUG LEVEL 1)", 1);
+	debug.log("error", "This is a simple test for debugging 2 (DEBUG LEVEL 2)", 2);
+	debug.log("warning", "This is a simple test for debugging 3 (DEBUG LEVEL 3)", 3);
+
+	PD_DEBUGLOG(debug, "information", "This is a complex PD_DEBUGLOG message");
+	PD_SIMPLE_DEBUGLOG("information", "This is a simple PD_SIMPLE_DEBUGLOG message");
 
 	return 0;
 }
