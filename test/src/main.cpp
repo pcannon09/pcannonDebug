5fd14922cc7eb79db2ab90d8b534be77bf86df0c
pcannon09 <pcannon775@gmail.com>
2025-07-07 22:29:02 +0200

    Set default settings and added example note for msg types

--- test/src/main.cpp
+++ test/src/main.cpp
@@ -5,20 +5,29 @@ int main()
 	pd::DebugSettings debugSettings;
 	debugSettings.debugID = true;
 	debugSettings.blockedSave = false;
-	debugSettings.timeStamp = true;
+	debugSettings.timeStamp = false;
 	debugSettings.timeZone = true;
 	debugSettings.output = true;
-
-	debugSettings.blockedSave = false;
 	debugSettings.custom = false;
+
+	// NOTE: CAN BE USED IF WANTED, UNCOMMENT THIS CODE TO USE THE FEATURE
+	/*
+	debugSettings.preStartMsg = "PRESTARTMSG - ";
+	debugSettings.startMsg = "STARTMSG - ";
+	
+	debugSettings.endMsg = "- ENDMSG ";
+	debugSettings.postEndMsg = "- POSTENDMSG ";
+	debugSettings.totalEndMsg = " - TOTAL END";
+	debugSettings.totalCustom = "Custom text for debug label";
+	*/
 
 	debugSettings.filePath = "./logs/log.log";
 
 	pd::Debug debug("main-debug", debugSettings);
 
-	debug.log("INFORMATION", "This is a simple test for debugging");
-	debug.log("ERROR", "This is a simple test for debugging 2");
-	debug.log("WARNING", "This is a simple test for debugging 3");
+	debug.log("information", "This is a simple test for debugging");
+	debug.log("error", "This is a simple test for debugging 2");
+	debug.log("warning", "This is a simple test for debugging 3");
 
 	return 0;
 }
