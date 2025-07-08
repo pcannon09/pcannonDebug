da1f2971901530ff79049fd72fd9c595a130344b
pcannon09 <pcannon775@gmail.com>
2025-07-08 12:42:41 +0200

    Set version to 1.0.0-build

--- inc/macros.hpp
+++ inc/macros.hpp
@@ -1,19 +1,19 @@
 #pragma once
 
 // Versioning
-#define PD_VERSION_MAJOR          0
+#define PD_VERSION_MAJOR          1
 #define PD_VERSION_MINOR          0
-#define PD_VERSION_PATCH          1
+#define PD_VERSION_PATCH          0
 
 // Version states:
 // dev - Develoment
 // build - Built Version
-#define PD_VERSION_STATE          "dev"
+#define PD_VERSION_STATE          "build"
 
-#define PD_VERSION                ((STT_VERSION_MAJOR<<16)|(STT_VERSION_MINOR<<8)|(STT_VERSION_PATCH)|(STT_VERSION_STATE << 24))
+#define PD_VERSION                ((PD_VERSION_MAJOR<<16)|(STT_VERSION_MINOR<<8)|(STT_VERSION_PATCH)|(STT_VERSION_STATE << 24))
 
-#define PD_VERSION_CHECK(STT_VERSION_MAJOR, STT_VERSION_MINOR, STT_VERSION_PATCH, STT_VERSION_STATE) \
-    ((PD_VERSION_MAJOR<<16)|(STT_VERSION_MINOR<<8)|(STT_VERSION_PATCH)|(STT_VERSION_STATE << 24))
+#define PD_VERSION_CHECK(PD_VERSION_MAJOR, PD_VERSION_MINOR, PD_VERSION_PATCH, PD_VERSION_STATE) \
+    ((PD_VERSION_MAJOR<<16)|(PD_VERSION_MINOR<<8)|(PD_VERSION_PATCH)|(STT_VERSION_STATE << 24))
 
 /**
  * @brief Debug log with file and line output capabilities using pd::Debug for debugging
@@ -36,7 +36,7 @@
 
 // Macro utils
 #define PD_STRINGIFY(x) #x
-#define PD_TOSTRING(x) STT_STRINGIFY(x)
+#define PD_TOSTRING(x) PD_STRINGIFY(x)
 
 #ifndef PD_DEV
 #	define PD_DEV true
